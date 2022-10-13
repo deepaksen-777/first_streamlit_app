1e826ff72becb788e5da9ec70a2e167281d81fff
deepaksen-777 <115613551+deepaksen-777@users.noreply.github.com>
2022-10-13 16:13:32 +0530

    Update streamlit_app.py
    Reading csv file and displaying the records in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,9 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+
+\\Read the CSV file
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
    
