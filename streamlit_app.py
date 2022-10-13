2cc33a67c56f84a9eaf28c0130db4234e702d438
deepaksen-777 <115613551+deepaksen-777@users.noreply.github.com>
2022-10-13 12:24:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My name is Deepak')
 streamlit.header('Breakfast Menu')
 streamlit.text('Bread & omelette')
+streamlit.text('Daal Roti')
+streamlit.text('Healthy food')
