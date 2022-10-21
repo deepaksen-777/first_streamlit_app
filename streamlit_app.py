eab12a2a2f824f37eb3dab3e819e0ea9aad661cd
deepaksen-777 <115613551+deepaksen-777@users.noreply.github.com>
2022-10-21 17:53:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ try:
          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
          # it will show data in table formate
          streamlit.dataframe(fruityvice_normalized)
+         
  except URLError as e:
      streamlit.error()
     
